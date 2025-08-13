32253ef50bfda9b28ca2cd431f237c2e8e83104f
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-08-14 01:33:25 +0200

    Remove old comment.

--- bevy_saga/src/lib.rs
+++ bevy_saga/src/lib.rs
@@ -12,7 +12,4 @@ pub use saga::Saga;
 pub use plugin::RegisterSaga;
 pub use result_handler::{OkStage, ErrStage};
 
-// Fallible Events that won't propagate. ?
-// BUG?: Sending the same event multiple times, then updating once (with chaining).
-
 pub trait SagaEvent: Event + Clone + SystemInput<Inner<'static> = Self> {}
