7be8e3a2622a5f95b17be93f278a7793fb4fc8d9
Victor Vanweyenberg <victor.vanweyenberg@enovates.com>
2025-08-19 16:30:40 +0200

    Implement event handlers using macro_rules and variadics.

--- bevy_saga/src/handler.rs
+++ bevy_saga/src/handler.rs
@@ -2,6 +2,7 @@ use crate::{plugin::BevySagaUtil, SagaEvent};
 use bevy::ecs::schedule::ScheduleConfigs;
 use bevy::ecs::system::ScheduleSystem;
 use bevy::prelude::{App, IntoScheduleConfigs, SystemParamFunction};
+use variadics_please::all_tuples;
 
 pub trait EventHandler<M> {
     type In: SagaEvent;
@@ -29,84 +30,29 @@ where
     }
 }
 
-impl<SPF1, SPF2, M1, M2, In> EventHandler<(M1, M2)> for (SPF1, SPF2)
-where
-    In: SagaEvent,
-    SPF1: SystemParamFunction<M1, In = In, Out = ()>,
-    SPF2: SystemParamFunction<M2, In = In, Out = ()>,
-    M1: 'static,
-    M2: 'static,
-{
-    type In = In;
+macro_rules! impl_event_handler {
+    ($(#[$meta:meta])* $(($SPF:ident, $spf:ident, $M:ident)),*) => {
+        impl<$($SPF,)* $($M,)* In> EventHandler<($($M,)*)> for ($($SPF,)*)
+        where
+            In: SagaEvent,
+            $($SPF: SystemParamFunction<$M, In = In, Out = ()>,)*
+            $($M: 'static,)*
+        {
+            type In = In;
 
-    fn register_handler(
-        self,
-        app: &mut App,
-    ) -> ScheduleConfigs<ScheduleSystem>
-    {
-        let (spf1, spf2) = self;
-        (
-            app.add_event_handler::<In, _>(spf1),
-            app.add_event_handler::<In, _>(spf2),
-        )
-            .into_configs()
+            fn register_handler(
+                self,
+                app: &mut App,
+            ) -> ScheduleConfigs<ScheduleSystem>
+            {
+                let ($($spf,)*) = self;
+                (
+                    $(app.add_event_handler::<In, _>($spf),)*
+                )
+                    .into_configs()
+            }
+        }
     }
 }
 
-impl<SPF1, SPF2, SPF3, M1, M2, M3, In> EventHandler<(M1, M2, M3)> for (SPF1, SPF2, SPF3)
-where
-    In: SagaEvent,
-    SPF1: SystemParamFunction<M1, In = In, Out = ()>,
-    SPF2: SystemParamFunction<M2, In = In, Out = ()>,
-    SPF3: SystemParamFunction<M3, In = In, Out = ()>,
-    M1: 'static,
-    M2: 'static,
-    M3: 'static,
-{
-    type In = In;
-
-    fn register_handler(
-        self,
-        app: &mut App,
-    ) -> ScheduleConfigs<ScheduleSystem>
-    {
-        let (spf1, spf2, spf3) = self;
-        (
-            app.add_event_handler::<In, _>(spf1),
-            app.add_event_handler::<In, _>(spf2),
-            app.add_event_handler::<In, _>(spf3),
-        )
-            .into_configs()
-    }
-}
-
-impl<SPF1, SPF2, SPF3, SPF4, M1, M2, M3, M4, In> EventHandler<(M1, M2, M3, M4)>
-    for (SPF1, SPF2, SPF3, SPF4)
-where
-    In: SagaEvent,
-    SPF1: SystemParamFunction<M1, In = In, Out = ()>,
-    SPF2: SystemParamFunction<M2, In = In, Out = ()>,
-    SPF3: SystemParamFunction<M3, In = In, Out = ()>,
-    SPF4: SystemParamFunction<M4, In = In, Out = ()>,
-    M1: 'static,
-    M2: 'static,
-    M3: 'static,
-    M4: 'static,
-{
-    type In = In;
-
-    fn register_handler(
-        self,
-        app: &mut App,
-    ) -> ScheduleConfigs<ScheduleSystem>
-    {
-        let (spf1, spf2, spf3, spf4) = self;
-        (
-            app.add_event_handler::<In, _>(spf1),
-            app.add_event_handler::<In, _>(spf2),
-            app.add_event_handler::<In, _>(spf3),
-            app.add_event_handler::<In, _>(spf4),
-        )
-            .into_configs()
-    }
-}
+all_tuples!(impl_event_handler, 2, 16, SPF, spf, M);
