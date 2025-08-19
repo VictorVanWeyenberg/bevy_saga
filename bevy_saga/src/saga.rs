3990a564e6a8d6d77671a96fac786308f5e0fb40
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-08-19 22:17:30 +0200

    Clean up saga generics.

--- bevy_saga/src/saga.rs
+++ bevy_saga/src/saga.rs
@@ -24,70 +24,70 @@ where
     }
 }
 
-impl<S1, S2, M1, M2, In> Saga<(M1, M2)> for (S1, S2)
+impl<S1, H, M1, MH, In> Saga<(M1, MH)> for (S1, H)
 where
-    In: SagaEvent,
     S1: EventProcessor<M1, In = In>,
-    S2: EventHandler<M2, In = S1::Out>,
-    S2::In: SagaEvent,
+    H: EventHandler<MH, In = S1::Out>,
+    In: SagaEvent,
+    H::In: SagaEvent,
 {
     type In = In;
 
     fn register(self, app: &mut App) -> ScheduleConfigs<ScheduleSystem> {
-        let (s1, s2) = self;
-        (s1.register_processor(app), s2.register_handler(app)).chain()
+        let (s1, h) = self;
+        (s1.register_processor(app), h.register_handler(app)).chain()
     }
 }
 
-impl<S1, S2, S3, M1, M2, M3, In> Saga<(M1, M2, M3)> for (S1, S2, S3)
+impl<S1, S2, H, M1, M2, MH, In> Saga<(M1, M2, MH)> for (S1, S2, H)
 where
-    In: SagaEvent,
     S1: EventProcessor<M1, In = In>,
     S2: EventProcessor<M2, In = S1::Out>,
-    S3: EventHandler<M3, In = S2::Out>,
+    H: EventHandler<MH, In = S2::Out>,
+    In: SagaEvent,
     S2::In: SagaEvent,
-    S3::In: SagaEvent,
+    H::In: SagaEvent,
 {
     type In = In;
 
     fn register(self, app: &mut App) -> ScheduleConfigs<ScheduleSystem> {
-        let (s1, s2, s3) = self;
+        let (s1, s2, h) = self;
         s1.register_processor(app);
         s2.register_processor(app);
-        s3.register_handler(app);
+        h.register_handler(app);
         (
             process_event::<S1::In, S1::Out>,
             process_event::<S2::In, S2::Out>,
-            handle_event::<S3::In>,
+            handle_event::<H::In>,
         )
             .chain()
     }
 }
 
-impl<S1, S2, S3, S4, M1, M2, M3, M4, In> Saga<(M1, M2, M3, M4)> for (S1, S2, S3, S4)
+impl<S1, S2, S3, H, M1, M2, M3, MH, In> Saga<(M1, M2, M3, MH)> for (S1, S2, S3, H)
 where
-    In: SagaEvent,
     S1: EventProcessor<M1, In = In>,
     S2: EventProcessor<M2, In = S1::Out>,
     S3: EventProcessor<M3, In = S2::Out>,
-    S4: EventHandler<M4, In = S3::Out>,
+    H: EventHandler<MH, In = S3::Out>,
+    In: SagaEvent,
     S2::In: SagaEvent,
     S3::In: SagaEvent,
-    S4::In: SagaEvent,
+    H::In: SagaEvent,
 {
     type In = In;
 
     fn register(self, app: &mut App) -> ScheduleConfigs<ScheduleSystem> {
-        let (s1, s2, s3, s4) = self;
+        let (s1, s2, s3, h) = self;
         s1.register_processor(app);
         s2.register_processor(app);
         s3.register_processor(app);
-        s4.register_handler(app);
+        h.register_handler(app);
         (
             process_event::<S1::In, S1::Out>,
             process_event::<S2::In, S2::Out>,
             process_event::<S3::In, S3::Out>,
-            handle_event::<S4::In>,
+            handle_event::<H::In>,
         )
             .chain()
     }
