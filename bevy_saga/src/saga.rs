f352c31054d95e896e942034ebe88a0cae65e01a
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-08-24 23:23:48 +0200

    Generate saga implementations using crabtime.

--- bevy_saga/src/saga.rs
+++ bevy_saga/src/saga.rs
@@ -1,9 +1,8 @@
-use crate::handler::EventHandler;
-use crate::processor::EventProcessor;
 use crate::SagaEvent;
+use crate::handler::EventHandler;
 use bevy::ecs::schedule::ScheduleConfigs;
 use bevy::ecs::system::ScheduleSystem;
-use bevy::prelude::{App, IntoScheduleConfigs};
+use bevy::prelude::App;
 
 pub trait Saga<M> {
     type In: SagaEvent;
@@ -23,64 +22,54 @@ where
     }
 }
 
-impl<S1, H, M1, MH, In> Saga<(M1, MH)> for (S1, H)
-where
-    S1: EventProcessor<M1, In = In>,
-    H: EventHandler<MH, In = S1::Out>,
-    In: SagaEvent,
-    H::In: SagaEvent,
-{
-    type In = In;
+#[crabtime::function]
+fn impl_saga() -> proc_macro2::TokenStream {
+    #![dependency(proc-macro2 = "1")]
+    #![dependency(quote = "1")]
 
-    fn register(self, app: &mut App) -> ScheduleConfigs<ScheduleSystem> {
-        let (s1, h) = self;
-        (s1.register_processor(app), h.register_handler(app)).chain()
+    use quote::{format_ident, quote};
+
+    let tokens = (1u32..16).map(|number_of_processors| {
+        let mut processor_generics = vec![];
+        let mut marker_generics = vec![];
+        let mut input_generics = vec![quote!(In)];
+        let mut unpack_variables = vec![];
+        for index in 1..(number_of_processors + 1) {
+            processor_generics.push(format_ident!("S{}", index));
+            marker_generics.push(format_ident!("M{}", index));
+            let previous_processor = format_ident!("S{}", index);
+            input_generics.push(quote!(#previous_processor::Out));
+            unpack_variables.push(format_ident!("s{}", index));
+        }
+        let handler_input = input_generics.remove(input_generics.len() - 1);
+
+        quote! {
+            impl<#(#processor_generics,)* H, #(#marker_generics,)* MH, In> crate::saga::Saga<(#(#marker_generics,)* MH)> for (#(#processor_generics,)* H)
+            where
+                #(#processor_generics: crate::processor::EventProcessor<#marker_generics, In = #input_generics>,)*
+                H: crate::handler::EventHandler<MH, In = #handler_input>,
+                In: crate::SagaEvent,
+                #(#processor_generics::In: crate::SagaEvent,)*
+            {
+                type In = In;
+
+                fn register(self, app: &mut bevy::prelude::App) -> bevy::ecs::schedule::ScheduleConfigs<bevy::ecs::system::ScheduleSystem> {
+                    let (#(#unpack_variables,)* h) = self;
+                    (
+                        #(#unpack_variables.register_processor(app),)*
+                        h.register_handler(app),
+                    )
+                        .chain()
+                }
+            }
+        }
+    }).collect::<Vec<_>>();
+
+    quote! {
+        use bevy::prelude::IntoScheduleConfigs;
+        #(#tokens)*
     }
 }
 
-impl<S1, S2, H, M1, M2, MH, In> Saga<(M1, M2, MH)> for (S1, S2, H)
-where
-    S1: EventProcessor<M1, In = In>,
-    S2: EventProcessor<M2, In = S1::Out>,
-    H: EventHandler<MH, In = S2::Out>,
-    In: SagaEvent,
-    S2::In: SagaEvent,
-    H::In: SagaEvent,
-{
-    type In = In;
 
-    fn register(self, app: &mut App) -> ScheduleConfigs<ScheduleSystem> {
-        let (s1, s2, h) = self;
-        (
-            s1.register_processor(app),
-            s2.register_processor(app),
-            h.register_handler(app),
-        )
-            .chain()
-    }
-}
-
-impl<S1, S2, S3, H, M1, M2, M3, MH, In> Saga<(M1, M2, M3, MH)> for (S1, S2, S3, H)
-where
-    S1: EventProcessor<M1, In = In>,
-    S2: EventProcessor<M2, In = S1::Out>,
-    S3: EventProcessor<M3, In = S2::Out>,
-    H: EventHandler<MH, In = S3::Out>,
-    In: SagaEvent,
-    S2::In: SagaEvent,
-    S3::In: SagaEvent,
-    H::In: SagaEvent,
-{
-    type In = In;
-
-    fn register(self, app: &mut App) -> ScheduleConfigs<ScheduleSystem> {
-        let (s1, s2, s3, h) = self;
-        (
-            s1.register_processor(app),
-            s2.register_processor(app),
-            s3.register_processor(app),
-            h.register_handler(app),
-        )
-            .chain()
-    }
-}
+impl_saga!();
---
+++ bevy_saga/tests/saga.rs
@@ -0,0 +1,143 @@
+use bevy::prelude::{App, ResMut, Resource, Update};
+use bevy_saga::RegisterSaga;
+use bevy_saga_macros::saga_event;
+
+#[test]
+fn big_big_saga_for_big_decisions() {
+    let mut app = App::new();
+    app.init_resource::<Counter>();
+    app.add_saga(Update, (f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16));
+    app.world_mut().send_event(Input);
+    app.update();
+    assert_eq!(16, app.world().resource::<Counter>().0);
+}
+
+#[derive(Default, Resource)]
+struct Counter(u8);
+
+#[saga_event]
+struct Input;
+
+#[saga_event]
+struct Intermediate1;
+
+#[saga_event]
+struct Intermediate2;
+
+#[saga_event]
+struct Intermediate3;
+
+#[saga_event]
+struct Intermediate4;
+
+#[saga_event]
+struct Intermediate5;
+
+#[saga_event]
+struct Intermediate6;
+
+#[saga_event]
+struct Intermediate7;
+
+#[saga_event]
+struct Intermediate8;
+
+#[saga_event]
+struct Intermediate9;
+
+#[saga_event]
+struct Intermediate10;
+
+#[saga_event]
+struct Intermediate11;
+
+#[saga_event]
+struct Intermediate12;
+
+#[saga_event]
+struct Intermediate13;
+
+#[saga_event]
+struct Intermediate14;
+
+#[saga_event]
+struct Output;
+
+fn f1(_: Input, mut counter: ResMut<Counter>) -> Intermediate1 {
+    counter.0 += 1;
+    Intermediate1
+}
+
+fn f2(_: Intermediate1, mut counter: ResMut<Counter>) -> Intermediate2 {
+    counter.0 += 1;
+    Intermediate2
+}
+
+fn f3(_: Intermediate2, mut counter: ResMut<Counter>) -> Intermediate3 {
+    counter.0 += 1;
+    Intermediate3
+}
+
+fn f4(_: Intermediate3, mut counter: ResMut<Counter>) -> Intermediate4 {
+    counter.0 += 1;
+    Intermediate4
+}
+
+fn f5(_: Intermediate4, mut counter: ResMut<Counter>) -> Intermediate5 {
+    counter.0 += 1;
+    Intermediate5
+}
+
+fn f6(_: Intermediate5, mut counter: ResMut<Counter>) -> Intermediate6 {
+    counter.0 += 1;
+    Intermediate6
+}
+
+fn f7(_: Intermediate6, mut counter: ResMut<Counter>) -> Intermediate7 {
+    counter.0 += 1;
+    Intermediate7
+}
+
+fn f8(_: Intermediate7, mut counter: ResMut<Counter>) -> Intermediate8 {
+    counter.0 += 1;
+    Intermediate8
+}
+
+fn f9(_: Intermediate8, mut counter: ResMut<Counter>) -> Intermediate9 {
+    counter.0 += 1;
+    Intermediate9
+}
+
+fn f10(_: Intermediate9, mut counter: ResMut<Counter>) -> Intermediate10 {
+    counter.0 += 1;
+    Intermediate10
+}
+
+fn f11(_: Intermediate10, mut counter: ResMut<Counter>) -> Intermediate11 {
+    counter.0 += 1;
+    Intermediate11
+}
+
+fn f12(_: Intermediate11, mut counter: ResMut<Counter>) -> Intermediate12 {
+    counter.0 += 1;
+    Intermediate12
+}
+
+fn f13(_: Intermediate12, mut counter: ResMut<Counter>) -> Intermediate13 {
+    counter.0 += 1;
+    Intermediate13
+}
+
+fn f14(_: Intermediate13, mut counter: ResMut<Counter>) -> Intermediate14 {
+    counter.0 += 1;
+    Intermediate14
+}
+
+fn f15(_: Intermediate14, mut counter: ResMut<Counter>) -> Output {
+    counter.0 += 1;
+    Output
+}
+
+fn f16(_: Output, mut counter: ResMut<Counter>) {
+    counter.0 += 1;
+}
