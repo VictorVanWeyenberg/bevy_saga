43566f99f6615005bc1984d9e1d217cf1e29e689
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-08-29 01:00:00 +0200

    Write doc.

--- bevy_saga_impl/src/processor.rs
+++ bevy_saga_impl/src/processor.rs
@@ -4,6 +4,47 @@ use bevy::ecs::system::ScheduleSystem;
 use bevy::prelude::{App, Event, IntoScheduleConfigs, SystemParamFunction};
 use variadics_please::all_tuples;
 
+/// The definition of an event processor.
+///
+/// An event processor is a collection of functions that all have the same type as input
+/// parameter. Only the first system in the collection has a return type. The following systems in
+/// the collection may not return anything. The return value of that first system will be passed on
+/// to the following processor or handler in the saga.
+///
+/// Both the input and output types have to be attributed with `saga_event`.
+/// 
+/// All systems in the collection are executed concurrently.
+///
+/// ```
+/// # use bevy::app::{App, Update};
+/// # use bevy::prelude::{Component, Query};
+/// # use bevy_saga_impl::SagaRegistry;
+/// # use bevy_saga_macros::saga_event;
+/// # #[derive(Component)]
+/// # struct Health;
+/// #[saga_event]
+/// struct A;
+///
+/// #[saga_event]
+/// struct B;
+///
+/// fn process_event(_: A, /* other queries or resources */) -> B { B }
+/// fn sibling1(_: A, health: Query<&Health>) { }
+/// fn sibling2(_: A) { }
+/// fn sibling3(_: A) { }
+///
+/// fn handler(_: B) { }
+///
+/// # let mut app = App::new();
+///
+/// // One event processor function is a valid event processor.
+/// let processor = process_event;
+/// app.add_saga(Update, (processor, handler));
+///
+/// // A processor can also be accompanied by other functions the handle the event.
+/// let processor = (process_event, sibling1, sibling2, sibling3);
+/// app.add_saga(Update, (processor, handler));
+/// ```
 pub trait EventProcessor<M> {
     type In: SagaEvent;
     type Out: Event;
