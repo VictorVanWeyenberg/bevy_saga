9d2e6d4a0c080714f06c9a9ac3b5f4b7e603e9e7
Victor Van Weyenberg <victorvanweyenb@gmail.com>
2025-08-29 23:52:30 +0200

    Write doc: SagaEvent.

--- bevy_saga_impl/src/lib.rs
+++ bevy_saga_impl/src/lib.rs
@@ -12,4 +12,13 @@ mod util;
 
 pub use extension::SagaRegistry;
 
+/// The trait type that propagates through your sagas.
+///
+/// You don't have to implement this trait directly. Simple use the `#[saga_event]` attribute to
+/// implement this trait.
+///
+/// Every type that is sent through a saga needs to implement this SagaEvent trait.
+///
+/// The attribute `#[saga_router]` indirectly also implements SagaEvent so you don't have to add
+/// the `#[saga_event]` attribute if your type is already attributed with `#[saga_router]`.
 pub trait SagaEvent: Event + Clone + SystemInput<Inner<'static> = Self> {}
